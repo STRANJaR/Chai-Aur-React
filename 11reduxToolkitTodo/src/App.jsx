c4f08b78e5e22c7bf4efa25f33b3cbb72ef14b4a
Rohit Shrivastav <rohitrks805@gmail.com>
2023-10-26 23:47:30 +0530

    React-Redux Toolkit

---
+++ 11reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Todos from './components/Todos'
+import AddTodo from './components/addTodo'
+
+function App() {
+  
+
+  return (
+    <>
+      <AddTodo />
+      <Todos/>
+    </>
+  )
+}
+
+export default App
