13704fafb579df11724c4b5dad9f98a4a6515dfd
Rohit Shrivastav <rohitrks805@gmail.com>
2023-11-13 23:55:59 +0530

    Comp. SignUp

---
+++ 12megaBlog/src/components/Select.jsx
@@ -0,0 +1,39 @@
+import React, {useId} from 'react'
+
+function Select({
+    options,
+    label,
+    className,
+    ...props
+}, ref) {
+
+    const id = useId()
+  return (
+    <div className='w-full'>
+        {label && <lable
+            htmlFor={id}
+            className = ''
+        >
+            </lable>}
+
+    <select
+    {...props}
+    ref={ref}
+    id={id}
+    className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+    >
+        {options?.map((option)=>(
+            <options
+            key={option}
+            value={option}
+            >
+                {option}
+
+            </options>
+        ))}
+    </select>
+    </div>
+  )
+}
+
+export default React.forwardRef(Select)
