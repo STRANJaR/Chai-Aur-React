b80f38edebd67474d714afbc1d47f65a3bfe175c
Rohit Shrivastav <rohitrks805@gmail.com>
2024-02-14 22:08:44 +0530

    copy to clipboard password

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
 
@@ -6,6 +6,11 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+
+
+  // useRef hook 
+
+  const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback(()=>{
     let pass = ""
@@ -25,6 +30,11 @@ function App() {
   },[length, numberAllowed, charAllowed, setPassword])
 
 
+  const copyPasswordToClipboard = useCallback(()=>{
+    passwordRef.current?.select( )
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
   useEffect(()=>{
     passwordGenerator()
   }, [length, numberAllowed, charAllowed, passwordGenerator])
@@ -39,10 +49,11 @@ function App() {
         className='outline-none w-full py-1 px-3'
         placeholder='password'
         readOnly
+        ref={passwordRef}
         />
 
         <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
-        
+        onClick={copyPasswordToClipboard}
         >Copy</button>
       </div>
 
